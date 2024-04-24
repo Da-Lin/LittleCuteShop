eacaf87b594fc4a12078d958b2f3fa2247699b26
Da Lin <alienld2014@gmail.com>
2024-04-24 01:04:49 -0400

    Added Vite with ESLint

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig(() => {
     return {
@@ -12,6 +13,6 @@ export default defineConfig(() => {
         build: {
             outDir: 'build',
         },
-        plugins: [react()],
+        plugins: [react(), eslint()],
     };
 });
