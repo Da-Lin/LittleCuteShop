b4cb0951fb6f6961ea7e306c847e91b440f5aebf
Da Lin <alienld2014@gmail.com>
2024-08-23 01:41:26 -0400

    Added pop up for explaining order status

--- src/components/order/orderCard.jsx
+++ src/components/order/orderCard.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Dialog, DialogActions, DialogTitle, Grid, IconButton, LinearProgress, Tooltip, Typography } from '@mui/material'
+import { Box, Button, Card, CardActions, CardContent, CardMedia, CircularProgress, Dialog, DialogActions, DialogTitle, Grid, IconButton, LinearProgress, Popover, Tooltip, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { OrderInfo } from './cart'
 import { useTranslation } from 'react-i18next'
@@ -35,7 +35,7 @@ export default function OrderCard({ orders }) {
                             <Typography >${order.totalPrice}</Typography>
                             <Typography variant="h6" color="primary">{t('order').cart.pickUpDate}: </Typography>
                             <Typography >{dayjs(order.pickUpDate.toDate()).format('YYYY-MM-DD')}</Typography>
-                            <Typography variant="h6" color="primary">Order Status: </Typography>
+                            <Typography variant="h6" color="primary">{t('order.orderStatus')}:<OrderStatusPopover /></Typography>
                             {order.status === "confirmed" ?
                                 <Typography color="green">{t('order.' + order.status)}</Typography>
                                 : <Typography color="red">{t('order.' + order.status)}</Typography>}
@@ -101,5 +101,57 @@ function CancelDialog({ openDialog, setOpenDialog, documentId, orders }) {
                 </Button>
             </DialogActions>
         </Dialog>
+    );
+}
+
+function OrderStatusPopover() {
+
+    const { t } = useTranslation()
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopoverOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handlePopoverClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+
+    return (
+        <Box sx={{ display: 'inline' }}>
+            <Typography
+                aria-owns={open ? 'mouse-over-popover' : undefined}
+                aria-haspopup="true"
+                onMouseEnter={handlePopoverOpen}
+                onMouseLeave={handlePopoverClose}
+                sx={{ display: 'inline' }}
+            >
+                (?)
+            </Typography>
+            <Popover
+                id="mouse-over-popover"
+                sx={{
+                    pointerEvents: 'none',
+                }}
+                open={open}
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                onClose={handlePopoverClose}
+                disableRestoreFocus
+            >
+                <Typography sx={{ p: 1 }}>{t('order.waitOrderStatusExplanation')}</Typography>
+                <Typography sx={{ p: 1 }}>{t('order.confirmedOrderStatusExplanation')}</Typography>
+            </Popover>
+        </Box>
     );
 }
