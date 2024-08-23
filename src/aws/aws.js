21ea074523f62aad0499c49c3857055bc36f7e73
Da Lin <alienld2014@gmail.com>
2024-08-23 15:56:44 -0400

    Upgrading to aws sdk v3

--- src/aws/aws.js
+++ src/aws/aws.js
@@ -1,12 +1,4 @@
-import * as AWS from "aws-sdk"
-
 import { SES } from "@aws-sdk/client-ses";
-
-AWS.config.update({
-    region: "us-east-1",
-    accessKeyId: import.meta.env.AWS_SES_LITTLE_CUTE_SHOP_ACCESS_KEY_ID,
-    secretAccessKey: import.meta.env.AWS_SES_LITTLE_CUTE_SHOP_SECRET_ACCESS_KEY
-})
 
 export const ses = new SES({
     region: "us-east-1",
