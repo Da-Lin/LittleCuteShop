135973e5bc4678ffc3166dd6061234b0394d66ca
Da Lin <alienld2014@gmail.com>
2024-09-06 19:16:28 -0400

    added complete order status

--- src/components/order/orderCard.jsx
+++ src/components/order/orderCard.jsx
@@ -34,9 +34,9 @@ export default function OrderCard({ orders, lastOrderRef }) {
                             <Typography variant="body1" color="primary">{t('order').cart.totalPrice}: <Typography variant="body2" color='black' sx={{ display: 'inline' }}>${order.totalPrice}</Typography></Typography>
                             <Typography variant="body1" color="primary">{t('order').cart.pickUpDate}: <Typography variant="body2" color='black' sx={{ display: 'inline' }}>{order.pickUpDate && dayjs(order.pickUpDate.toDate()).format('YYYY-MM-DD')}</Typography></Typography>
                             <Typography variant="body1" color="primary">{t('order.orderStatus')}<OrderStatusPopover />:
-                                {order.status === "confirmed" ?
-                                    <Typography variant="body2" color="green" sx={{ display: 'inline' }}>{t('order.' + order.status)}</Typography>
-                                    : <Typography variant="body2" color="red" sx={{ display: 'inline' }}>{t('order.' + order.status)}</Typography>}
+                                {order.status === "confirmed" || order.status === "complete"?
+                                    <Typography variant="body2" color="green" sx={{ display: 'inline' }}> {t('order.' + order.status)}</Typography>
+                                    : <Typography variant="body2" color="red" sx={{ display: 'inline' }}> {t('order.' + order.status)}</Typography>}
                             </Typography>
                         </CardContent>
                         <CardActions ref={index === orders.length - 1 ? lastOrderRef : undefined}>
